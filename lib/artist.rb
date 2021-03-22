e34a6e5c42e8624c6fc4fffa745696e48a98a520
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-03-21 20:38:50 -0400

    commit tree 205c70f0cdc41b83b01d33fb0e43110f3c6608ee

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,35 @@
 
+class Artist
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        #@genre = genre
+        @@all << self
+    end
+
+    def self.all 
+        @@all 
+    end
+
+    def songs
+        Song.all.select do |song|
+            song.artist == self
+        end
+    end
+
+    def new_song(title, genre)
+        Song.new(title, self, genre)
+    end
+
+    def genres
+        Song.all.map do |song|
+            song.genre
+        end
+    end
+
+
+
+end
